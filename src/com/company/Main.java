a7e60f96dacd6255b0591866a25072f5ed67e245
Magnus Jonsson <jonsson.magnus@outlook.com>
2018-08-03 13:58:02 +0200

    adderat dividerat och add

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner input1 = new Scanner (System.in);
+        Scanner input2 = new Scanner (System.in);
+        int xvalue = input1.nextInt();
+        int yvalue = input2.nextInt();
+        /*Integer.parseInt(input1.nextLine());
+        Integer.parseInt(input2.nextLine());*/
+        dividedBy delat = new dividedBy();
+        delat.divided(xvalue, yvalue);
+
+        add adderat = new add();
+        adderat.add(xvalue, yvalue);
+
     }
 }
---
+++ src/com/company/dividedBy.java
@@ -0,0 +1,9 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class dividedBy {
+    public int divided(int x, int y) {
+        return x / y;
+    }
+}
